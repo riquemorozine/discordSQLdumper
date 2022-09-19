1c6a3e83bb59e1d1ccc29c7d0634bcc113343504
MeliodasSxX <66651356+meliodas0001@users.noreply.github.com>
2022-09-19 20:45:52 -0300

    first commit

---
+++ src/index.ts
@@ -0,0 +1,29 @@
+import "dotenv/config";
+import { Client, Attachment } from "discord.js";
+import ms from "ms";
+
+const client = new Client({ intents: 32767 });
+
+import { SqlDumper } from "./utils/dumperSQL";
+import { channelDumpId, timeToDump } from "./config.json";
+
+client.on("ready", () => {
+  console.log("Dumper On");
+
+  const channell = client.channels.cache.get(channelDumpId);
+
+  setInterval(() => {
+    SqlDumper();
+
+    setTimeout(async () => {
+      if (channell.isTextBased()) {
+        channell.send({
+          content: `${new Date()}`,
+          files: ["./src/sql/zumbilandia.sql"],
+        });
+      }
+    }, ms("10s"));
+  }, ms(timeToDump));
+});
+
+client.login(process.env.TOKEN);
---
+++ src/utils/dumperSQL.ts
@@ -0,0 +1,17 @@
+import mysqldump from "mysqldump";
+
+export function SqlDumper() {
+  try {
+    mysqldump({
+      connection: {
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+      },
+      dumpToFile: `./src/sql/zumbilandia.sql`,
+    });
+  } catch (err) {
+    console.log(err);
+  }
+}
